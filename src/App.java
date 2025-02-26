caa51c9702d0593e8ee397a5c88251b89723390c
jasper <whiteana001@warren.k12.in.us>
2025-02-26 13:29:30 -0500

    initial commit

--- src/App.java
+++ src/App.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class App {
+    public static void main(String[] args) {
+        var numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        var oddNumbers = numbers.stream()
+                .filter(number -> number % 2 !=0 )
+                .toList();
+
+        System.out.print("Odd numbers: " + oddNumbers);
+
+    }
 }
